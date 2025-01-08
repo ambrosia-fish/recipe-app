03c11d0e73f5b52db1f317d7576994cdf804ed83
ambrosia-fish <148804322+ambrosia-fish@users.noreply.github.com>
2025-01-08 16:29:05 -0500

    Added login, logout, protection

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -2,14 +2,17 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Recipe 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
     template_name = 'recipes/main.html'
 
-class RecipeDetailView(DetailView):                       #class-based view
-   model = Recipe                                        #specify model
+class RecipeDetailView(LoginRequiredMixin, DetailView):                       
+   model = Recipe                                        
    template_name = 'recipes/detail.html'                 
 
+# @login_required
 def recipe_home(request):
     return render(request,'recipes/recipes_home.html')
