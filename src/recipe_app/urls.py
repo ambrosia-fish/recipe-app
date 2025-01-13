4927c22c658402a001d63e402db41cfa24311c61
ambrosia-fish <148804322+ambrosia-fish@users.noreply.github.com>
2025-01-13 14:19:28 -0500

    consolidate styles into styles.css

--- src/recipe_app/urls.py
+++ src/recipe_app/urls.py
@@ -14,4 +14,7 @@ urlpatterns = [
     path("success/", success_view, name="success"),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Add static files urlpatterns
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
