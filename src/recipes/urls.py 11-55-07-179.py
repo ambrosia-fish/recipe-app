505cbf8d0fadeaa0c8d764b386dae37b1739218b
ambrosia-fish <148804322+ambrosia-fish@users.noreply.github.com>
2025-01-30 14:57:46 -0500

    And fonts and updated styles

---
+++ src/recipes/urls.py 11-55-07-179.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import RecipeListView, RecipeDetailView, recipe_home, RecipeAnalyticsView
+from . import views 
+
+app_name = "recipes"
+
+urlpatterns = [
+    path("", recipe_home, name="home"),  # Home page
+    path("list/", RecipeListView.as_view(), name="list"),
+    path("list/<pk>/", RecipeDetailView.as_view(), name="recipe_detail"),
+    path("data/", RecipeAnalyticsView.as_view(), name="analytics"),
+    path("my-recipes/", views.my_recipes, name="my_recipes"),
+    path("save-recipe/<int:recipe_id>/", views.save_recipe, name="save_recipe"),
+]
